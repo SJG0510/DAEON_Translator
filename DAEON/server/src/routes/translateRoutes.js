7d27fda99b87118794a9593d75ebbc93e7edacad
SJG0510 <sjs5382352@gmail.com>
2025-11-25 16:49:43 +0900

    Delete DAEON directory

--- DAEON/server/src/routes/translateRoutes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/translateRoutes.js
-const express = require("express");
-const router = express.Router();
-const { translateHandler } = require("../controllers/translateController");
-
-// POST /api/translate
-router.post("/", translateHandler);
-
-module.exports = router;
